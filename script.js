0c585f05b38f6756d53fd0b024a8b22409b9ad66
Abhay7995 <abhay798599@gmail.com>
2024-12-27 13:07:55 +0530

    java1

---
+++ script.js
@@ -0,0 +1,193 @@
+// function myfunction(){
+//     var carname="Welcome!";
+//     document.getElementById("demo").innerHTML=carname;
+// }
+
+
+// var a=10;
+// function functionA(){
+//     console.log("Start Function A");
+//     function functionB(){
+//         console.log("In function B");
+//     }
+//     functionB();
+// }
+// functionA();
+// console.log("Global Context");
+
+
+// var name1='john';
+
+// function first(){
+//     var a='Helllo';
+//     second();
+//     var x=a+name1;
+//     console.log(x);
+// }
+// function second(){
+//     var b='Hi!';
+//     third();
+//     var z=b+name1;
+//     console.log(z);
+// }
+// function third(){
+//     var c='Hey!';
+//     var z=c+name1;
+//     console.log(z);
+// }
+// first();
+
+
+
+// var a=5;
+// function add(){
+//     var b= 45;
+//     console.log(a+b);
+//     function mul(){
+//         var c=45;
+//         console.log(c*c);
+//     }
+//     mul();
+
+// }
+// add();
+
+
+
+
+// var age=20;
+// if(age>18){
+//     document.write("<b> You are eligible to cast the vote.</b>");
+// }
+
+
+// for (var i=0;i<100;i++){
+//     if (i%2==0
+// }
+
+
+
+
+// function myfunction(){
+//     alert("Hello World")
+// }
+
+
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" years old.  ");
+
+// }
+
+// var res=mul(12,50);
+// function mul(x,y){
+//     return x*y;
+// }
+// document.write(res);
+
+// function myFunction(g1,g2){
+//     return g1/g2;
+
+// }
+// value=myFunction(8,2);
+// console.log(value);
+
+
+// function add(a,b){
+//     console.log(a+b);
+// };
+// add(10,20);
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// };
+// add2(102,20);
+// var add3=(a,b)=> c;
+
+// function myFunction(){
+//     var x=5;
+//     var y="5"+5;
+//     var z="Hello";
+//     var demo=document.getElementById("demo");
+//     demo.innerHTML=x+"<br>"+y+"<br>"+z;
+
+// };
+
+
+// function information (firstName,lastname,language){
+//     if(arguments.length===3){
+//         console.log(firstName);
+//         console.log(lastname);
+//         console.log(language);
+//     }
+// }
+// information();
+// information('abhay ','ankit','ankur');
+
+// function sayHello(){
+//     alert("Hello World")
+// }
+
+// function over(){
+//     document.getElementById("mytext").style.color="red";
+// }
+// function mount(){
+//     document.getElementById("mytext").style.color="green";
+
+// }
+
+
+// var retVal=confirm("DO you want to Continue ?");if(retVal==true){
+//     alert("User wants to continue!");
+// }else{
+//     alert("User does not want to continue!");
+// }
+
+
+// var retVal=prompt("Enter your name:","your name here");
+// alert("You have entered:" + retVal);
+
+
+// var name=prompt("Enter your Name","Name");
+// document.write("<h2>Hello   "+name+"</h2>"); 
+
+
+
+// var question="What is 10+10?";
+// var answer =20;
+
+// var correct='<img src="correct.jpg" alt="correct" height="150" width="150">';
+// var incorrect='<img src="incorrect.jpg" alt="incorrect" height="150" width="150">';
+// var response=prompt(question,"0");
+// for(count=0;count<3;count++){
+//     if(response!=answer){
+//         confirm("Wrong Press Ok for another choice");
+//         response=prompt(question,"0");
+//         if(count==2){
+//             alert("Better luck next time");
+//         }
+//     }else{
+//             alert("Great! you are right");
+//             count=3;
+//         }  
+// }
+// var output=(response==answer)?correct:incorrect;
+// document.write("<br>");
+// document.write(output);
+
+
+    // function redirection(){
+    //     window.location="https://www.instagram.com";
+
+    // }
+    // document.write("You will be redirected to main page in 5 sec");
+    // setTimeout('redirection()',5000);
+
+
+
+    document.addressform.value;
+    function validateform(){
+        var x=document.forms.addressform.value;
+        if(x==null || x=="" ){
+            alert("First name must befill");
+            return false;
+        }
+    }
