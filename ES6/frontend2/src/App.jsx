000d317230016b2d2bc52eb4d2145ce60481d603
Abhay7995 <abhay798599@gmail.com>
2025-11-03 13:06:47 +0530

    book

--- ES6/frontend2/src/App.jsx
+++ ES6/frontend2/src/App.jsx
@@ -59,27 +59,43 @@
 // export default App;
 
 
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+//   const [counter, setCounter] = useState(0)
+
+//   const addValue = () => {
+//     if (counter < 20) setCounter(counter + 1)
+//   }
+
+//   const removeValue = () => {
+//     if (counter > 0) setCounter(counter - 1)
+//   }
+
+//   return (
+//     <>
+//       <h1>College React</h1>
+//       <h2>Counter Value: {counter}</h2>
+//       <button onClick={addValue}>Add Value</button><br />
+//       <button onClick={removeValue}>Remove Value</button>
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import Book from "./Book";
+
+
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  const addValue = () => {
-    if (counter < 20) setCounter(counter + 1)
-  }
-
-  const removeValue = () => {
-    if (counter > 0) setCounter(counter - 1)
-  }
-
   return (
-    <>
-      <h1>College React</h1>
-      <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button><br />
-      <button onClick={removeValue}>Remove Value</button>
-    </>
+    <div>
+      <Book />
+      <Book />
+      <Book />
+    </div>
   )
 }
 
